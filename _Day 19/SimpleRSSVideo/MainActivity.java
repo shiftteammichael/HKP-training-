e85cde00ae3322fa855f0c136d46eddfb93b146d
shiftteammichael <46112887+shiftteammichael@users.noreply.github.com>
2021-01-08 19:45:24 -0500

    Add files via upload

---
+++ _Day 19/SimpleRSSVideo/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.simplerssvideo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Xml;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lvRss;
+    ArrayList<String>titles;
+    ArrayList<String> links;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvRss=(ListView) findViewById(R.id.lvRss);
+        lvRss.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Uri uri=Uri.parse(links.get(position));
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+        new ProcessInBackground().execute();
+    }
+    public InputStream getInputStream(URL url) throws IOException {
+        try{
+            return url.openConnection().getInputStream();
+        }catch(IOException e)
+        {
+            return null;
+        }
+    }
+    public class ProcessInBackground extends AsyncTask<Integer,Integer,String> {
+        ProgressDialog progressDialog=new ProgressDialog(MainActivity.this);
+        Exception exception=null;
+        @Override
+        protected String doInBackground(Integer... integers) {
+            try{
+                URL url= new URL("https://feeds.news24.com/articles/fin24/tech/rss");
+                XmlPullParserFactory factory= XmlPullParserFactory.newInstance();
+                factory.setNamespaceAware(false);
+                XmlPullParser xpp= factory.newPullParser();
+                xpp.setInput(getInputStream(url),"UTF-8");
+                boolean insideItem=false;
+                int eventType=xpp.getEventType();
+                while(eventType!= XmlPullParser.END_DOCUMENT){
+                    if(eventType==XmlPullParser.START_TAG){
+                        if(xpp.getName().equalsIgnoreCase("item")){
+                            insideItem=true;
+                        }
+                        else if(xpp.getName().equalsIgnoreCase("title")){
+                            if(insideItem){
+                                titles.add(xpp.nextText());
+                            }
+                        }
+                    }
+                    else if(eventType==XmlPullParser.END_TAG && xpp.getName().equalsIgnoreCase("item")){
+                        insideItem=false;
+                    }
+                    eventType=xpp.next();
+                }
+            }
+            catch(MalformedURLException e){
+                exception=e;
+            }
+            catch(XmlPullParserException e){
+                exception=e;
+            }
+            catch(IOException e){
+                exception=e;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog.setMessage("Busy loading rss feed... please wait...");
+            progressDialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            ArrayAdapter<String>adapter= new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,titles);
+            lvRss.setAdapter(adapter);
+            progressDialog.dismiss();
+        }
+    }
+}
